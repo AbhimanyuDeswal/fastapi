236a8960698dca7b37a4741d586b9d55a480aaa4
AbhimanyuDeswal <abhimanyudeswal07@gmail.com>
2023-01-05 12:06:23 +0530

    use pydantic model to validate the input data in post request method

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 from fastapi import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.post("/createpost")
-async def create_post(payload:dict = Body(...)):
-    print(payload)
-    return {"body": f"{payload}"}
+async def create_post(post:Post):
+    return {"body": post}
